7fb18771eb536ff13b64170b2880203868a11bdd
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-11 20:13:28 -0700

    Confused myself, atom creates files out of nowhere sometimes

--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
 var code = require('../main');
-  var expect = require('chai').expect;
+var expect = require('chai').expect;
 
-  describe('', function() {
-    it('');
+describe('Leap year', function() {
+  it('Should return true on values that are evenly divisable by 4', function() {
+     expect(code.leap()).to.equal("true");
+     expect(code.leap()).to.equal("true");
+  });
+  // it('Should return false on values that are divisable by 100 but not 400');
+  // it('should return true');
+  // it('should return false');
 });
